c242ea56378d0b14f84b4d35f977a77a1abfabe0
jackmcc08 <jackmcc08@gmail.com>
2021-05-04 18:17:35 +0100

    created a record class to extract records out of the account class

---
+++ lib/record.rb
@@ -0,0 +1,8 @@
+class Record
+  def initialize(amount, type, date, balance)
+    @amount = amount
+    @type = type
+    @date = date
+    @balance = balance
+  end
+end
---
+++ spec/record_spec.rb
@@ -0,0 +1,18 @@
+require 'record'
+
+describe Record do
+  let(:test_recorder) { Record.new }
+
+  describe '#new' do
+    it 'creates a record object with the appropriate input' do
+      amount_input = 1000
+      type_input = "deposit"
+      date_input = "01/01/2021"
+      balance_input = 1000
+
+      test_record = Record.new(amount_input, type_input, date_input, balance_input)
+
+      expect(test_record).to be_instance_of Record
+    end
+  end
+end
